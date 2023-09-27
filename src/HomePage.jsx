e0363eb66fb057c84189c831a72c2b8d44b8142e
PravasTheDeveloper <info.pravas.cs@gmail.com>
2023-09-27 16:55:47 +0100

    workied

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,9 +1,18 @@
+import axios from 'axios'
 import React from 'react'
 
 function HomePage() {
+
+  const testRoute = async() => {
+    const res = await axios.get("/api")
+
+    console.log(res)
+  }
+
   return (
     <div>
       HOme
+      <button onClick={testRoute}>Click</button>
     </div>
   )
 }
